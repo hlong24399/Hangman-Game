b9d1e8421018b8d32c5234ee9cf1ad55a8edf851
LoganNguyen <54257351+hlong24399@users.noreply.github.com>
2020-01-29 19:52:46 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <vector>
 using namespace std;
 
@@ -46,6 +47,91 @@ public:
 		return false;
 	}
 
+	void hanging(int num) {
+		cout << endl;
+		switch (num) {
+		case 0:
+			cout << setw(10) << "" << "  ____________" << endl;
+			cout << setw(10) << "" << " |           |" << endl;
+			cout << setw(10) << "" << " |           |" << endl;
+			cout << setw(10) << "" << " |           " << endl;
+			cout << setw(10) << "" << " |			" << endl;
+			cout << setw(10) << "" << " |			" << endl;
+			cout << setw(10) << "" << " |			" << endl;
+			cout << setw(10) << "" << " |			" << endl;
+			cout << setw(10) << "" << "/ \\			" << endl;
+			break;
+		case 1:
+			cout << setw(10) << "" << "  ____________" << endl;
+			cout << setw(10) << "" << " |           |" << endl;
+			cout << setw(10) << "" << " |           |" << endl;
+			cout << setw(10) << "" << " |           O" << endl;
+			cout << setw(10) << "" << " |			" << endl;
+			cout << setw(10) << "" << " |			" << endl;
+			cout << setw(10) << "" << " |			" << endl;
+			cout << setw(10) << "" << " |			" << endl;
+			cout << setw(10) << "" << "/ \\			" << endl;
+			break;
+		case 2:
+			cout << setw(10) << "" << "  ____________" << endl;
+			cout << setw(10) << "" << " |           |" << endl;
+			cout << setw(10) << "" << " |           |" << endl;
+			cout << setw(10) << "" << " |           O" << endl;
+			cout << setw(10) << "" << " |          /|" << endl;
+			cout << setw(10) << "" << " |			" << endl;
+			cout << setw(10) << "" << " |			" << endl;
+			cout << setw(10) << "" << " |			" << endl;
+			cout << setw(10) << "" << "/ \\			" << endl;
+			break;
+		case 3:
+			cout << setw(10) << "" << "  ____________" << endl;
+			cout << setw(10) << "" << " |           |" << endl;
+			cout << setw(10) << "" << " |           |" << endl;
+			cout << setw(10) << "" << " |           O" << endl;
+			cout << setw(10) << "" << " |          /|\\" << endl;
+			cout << setw(10) << "" << " |			" << endl;
+			cout << setw(10) << "" << " |			" << endl;
+			cout << setw(10) << "" << " |			" << endl;
+			cout << setw(10) << "" << "/ \\			" << endl;
+			break;
+		case 4:
+			cout << setw(10) << "" << "  ____________" << endl;
+			cout << setw(10) << "" << " |           |" << endl;
+			cout << setw(10) << "" << " |           |" << endl;
+			cout << setw(10) << "" << " |           O" << endl;
+			cout << setw(10) << "" << " |          /|\\" << endl;
+			cout << setw(10) << "" << " |           |" << endl;
+			cout << setw(10) << "" << " |			" << endl;
+			cout << setw(10) << "" << " |			" << endl;
+			cout << setw(10) << "" << "/ \\		" << endl;
+			break;
+		case 5:
+			cout << setw(10) << "" << "  ____________" << endl;
+			cout << setw(10) << "" << " |           |" << endl;
+			cout << setw(10) << "" << " |           |" << endl;;
+			cout << setw(10) << "" << " |           O" << endl;
+			cout << setw(10) << "" << " |          /|\\" << endl;
+			cout << setw(10) << "" << " |           |" << endl;
+			cout << setw(10) << "" << " |          /" << endl;
+			cout << setw(10) << "" << " |			" << endl;
+			cout << setw(10) << "" << "/ \\			" << endl;
+			break;
+		case 6:
+			cout << setw(10) << "" << "  ____________" << endl;
+			cout << setw(10) << "" << " |           |" << endl;
+			cout << setw(10) << "" << " |           |" << endl;;
+			cout << setw(10) << "" << " |           O" << endl;
+			cout << setw(10) << "" << " |          /|\\" << endl;
+			cout << setw(10) << "" << " |           |" << endl;
+			cout << setw(10) << "" << " |          / \\" << endl;
+			cout << setw(10) << "" << " |			" << endl;
+			cout << setw(10) << "" << "/ \\         " << endl;
+			break;
+		}
+
+
+	}
+
 	void run(string k) {
 		key = k;
 		answer.resize(key.size());
@@ -56,13 +142,37 @@ public:
 		cout << " Here are unchosen letters " << endl;
 		Alphabet();
 		cout << " Please enter the letter that you choose: ";
-		while (isFilled()) {
+		int losingCount = 0;
+		hanging(0);
+		int winningCount = 0;
+		while (isFilled() && losingCount != 6) {
 			char select;
 			cin >> select;
 			Alphabet(select);
 			Alphabet();
+			
 			if (isIncluded(select)) {
-				cout << " Good ";
+				switch (winningCount) {
+				case 0:
+					cout << " Good " << endl;
+					break;
+				case 1:
+					cout << " Eh, not bad. " << endl;
+					break;
+				case 2:
+					cout << " Yo! That's brilliant!" << endl;
+					break;
+				case 3:
+					cout << " No kidding, y'all!!! " << endl;
+					break;
+				case 4:
+					cout << " God damn! I can't imagine! *passed out* " << endl;
+					break;
+
+				}
+				winningCount++;
+				
+				cout << endl;
 				addLetter(select);
 				for (auto i : answer) {
 					cout << i << "  ";
@@ -70,21 +180,23 @@ public:
 				cout << endl;
 			}
 			else {
+				///Something here as hangman
+				losingCount++;
+				hanging(losingCount);
 				for (auto i : answer) {
 					cout << i << "  ";
 				}
 				cout << endl;
 			}
 		}
-		cout << " Wonderful !! You are the first person who completes this game, which means that you must be Alice!";
-		cout << endl;
+		cout << setw(10) << "" << " Well, the game is done. HOWEVER!!! Wanna try again? Kiss that guy twice  " << endl;
 	}
 };
 
 
 int main() {
 	HangGame game1;
-	cout << " Enter the keyword ";
+	cout << " Enter the key word ";
 	string key;
 	cin >> key;
 	game1.run(key);
